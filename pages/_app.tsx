841c24aa9a4467e6c66a04eec0d37af32917ad92
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-01-10 11:39:01 -0500

    Remove refetch behavior on queryClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import "reflect-metadata";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
